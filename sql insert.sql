88b57a4cbaa6fbb6901036bccbfcfac7a01727e4
Soledad Carrasco <zhole03@gmail.com>
2022-04-24 17:01:03 -0500

    new sql scripts

--- sql insert.sql
+++ sql insert.sql
@@ -8,6 +8,19 @@ insert into TM_USER(USERNAME, PASSWORD, NAME, LASTNAME, BIRTH,PHONE, EMAIL) valu
 go
 
 
+insert into TM_USER(USERNAME, PASSWORD, NAME, LASTNAME, BIRTH,PHONE, EMAIL) values('mescobar','147852','Martin','Escobar','1998-02-23', '947856321','mescobar@envios.pe'); 
+go
+
+insert into TM_USER(USERNAME, PASSWORD, NAME, LASTNAME, BIRTH,PHONE, EMAIL) values('druiz','abcdef','Daniel','Ruiz','1970-02-23', '947856541','druiz@envios.pe'); 
+go
+
+insert into TM_USER(USERNAME, PASSWORD, NAME, LASTNAME, BIRTH,PHONE, EMAIL) values('cquispe','abcdef','Christian','Quispe','1995-02-23', '94754623','cquispe@envios.pe'); 
+go
+
+insert into TM_USER(USERNAME, PASSWORD, NAME, LASTNAME, BIRTH,PHONE, EMAIL) values('mcarrasco','Server001','Soledad','Carrasco','1996-05-13', '947986231','mcarrasco@envios.pe'); 
+go
+
+
 //////////////////////// REGISTRO DE ENVIOS /////////////////////////
 
 insert into TM_ENVIO (COD_ORDEN, IDUSER, SRC_ADD, DEST_ADD, PRICE, WEIGHT, PAQUETE, ESTADO) 
@@ -20,7 +33,14 @@ go
 insert into TM_ENVIO(COD_ORDEN, IDUSER, SRC_ADD, DEST_ADD, PRICE, WEIGHT, PAQUETE, ESTADO) values('C-0003',2,'VMT','Comas', 90, 14,'Cuadernos',5); 
 go
 
+insert into TM_ENVIO(COD_ORDEN, IDUSER, SRC_ADD, DEST_ADD, PRICE, WEIGHT, PAQUETE, ESTADO) values('C-0004',6,'Lince','Miraflores', 50, 20,'Papeles',4); 
+go
 
+insert into TM_ENVIO(COD_ORDEN, IDUSER, SRC_ADD, DEST_ADD, PRICE, WEIGHT, PAQUETE, ESTADO) values('C-0005',3,'Ate','Surco', 100, 15,'Laptop',2); 
+go
+
+insert into TM_ENVIO(COD_ORDEN, IDUSER, SRC_ADD, DEST_ADD, PRICE, WEIGHT, PAQUETE, ESTADO) values('C-0006',5,'Surquillo','Lurin', 54, 23,'Folders',5); 
+go
 
 ///////////////////// ESTADOS DE ENVIO /////////////////////////////////
 
