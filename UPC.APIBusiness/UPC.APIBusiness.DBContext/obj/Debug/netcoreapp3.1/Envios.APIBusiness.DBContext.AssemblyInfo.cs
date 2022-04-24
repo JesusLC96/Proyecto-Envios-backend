e2d46cc577e5d48015ab868e46fcf3d0e479c4e6
JesusLC96 <jesuslc96@gmail.com>
2022-04-24 15:02:20 -0500

    v3

--- UPC.APIBusiness/UPC.APIBusiness.DBContext/obj/Debug/netcoreapp3.1/Envios.APIBusiness.DBContext.AssemblyInfo.cs
+++ UPC.APIBusiness/UPC.APIBusiness.DBContext/obj/Debug/netcoreapp3.1/Envios.APIBusiness.DBContext.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Envios.APIBusiness.DBContext")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("UPC.APIBusiness.DBContext")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Envios.APIBusiness.DBContext")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Envios.APIBusiness.DBContext")]
+[assembly: System.Reflection.AssemblyProductAttribute("UPC.APIBusiness.DBContext")]
+[assembly: System.Reflection.AssemblyTitleAttribute("UPC.APIBusiness.DBContext")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
